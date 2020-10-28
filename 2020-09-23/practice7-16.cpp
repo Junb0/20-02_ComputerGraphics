023f9a834debd516cde0dd76939fb5f0495aba1d
Junyeong Park <junb0sday@gmail.com>
2020-10-29 04:37:35 +0900

    7-16 진짜 완성

--- 2020-09-23/practice7-16.cpp
+++ 2020-09-23/practice7-16.cpp
@@ -38,7 +38,7 @@
 #define v8 {1.0f, 1.0f, 1.0f}
 #define v9 {0.5f, 1.0f, 0.5f}
 #define rndclr {urdColor(dre), urdColor(dre), urdColor(dre)}
-#define CLR(a) {clr[a][0], clr[a][1], clr[a][2]}
+#define CLR(a) {clr[a - 1][0], clr[a - 1][1], clr[a - 1][2]}
 
 using namespace std;
 
@@ -50,7 +50,6 @@ GLvoid InitShader();
 GLvoid UpdateBuffer();
 GLvoid Timer(int value);
 GLvoid Keyboard(unsigned char key, int x, int y);
-GLvoid SpecialKey(int key, int x, int y);
 
 GLuint VAO[3], VBO[3];
 
@@ -59,8 +58,8 @@ default_random_engine dre(rd());
 uniform_real_distribution<> urd1(-0.7f, 0.7f);
 uniform_real_distribution<> urd2(0.005f, 0.02f);
 uniform_real_distribution<> urdColor(0.0f, 1.0f);
-GLfloat clr[9][3] = {
-	rndclr, rndclr,rndclr,rndclr,rndclr,rndclr,rndclr,rndclr,rndclr
+GLfloat clr[6][3] = {
+	rndclr,rndclr,rndclr,rndclr,rndclr,rndclr
 };
 
 char* filetobuf(const char* file) {
@@ -82,35 +81,42 @@ char* filetobuf(const char* file) {
 	return buf;
 }
 
-GLfloat Cube[12][3][2][3] = {
-	{{v1, CLR(1)}, {v7, CLR(7)}, {v5, CLR(5)}},
-	{{v1, CLR(1)}, {v3, CLR(3)}, {v7, CLR(7)}},
-	{{v1, CLR(1)}, {v4, CLR(4)}, {v3, CLR(3)}},
-	{{v1, CLR(1)}, {v2, CLR(2)}, {v4, CLR(4)}},
-	{{v3, CLR(3)}, {v8, CLR(8)}, {v7, CLR(7)}},
-	{{v3, CLR(3)}, {v4, CLR(4)}, {v8, CLR(8)}},
-	{{v5, CLR(5)}, {v7, CLR(7)}, {v8, CLR(8)}},
-	{{v5, CLR(5)}, {v8, CLR(8)}, {v6, CLR(6)}},
-	{{v1, CLR(1)}, {v5, CLR(5)}, {v6, CLR(6)}},
-	{{v1, CLR(1)}, {v6, CLR(6)}, {v2, CLR(2)}},
-	{{v2, CLR(2)}, {v6, CLR(6)}, {v8, CLR(8)}},
-	{{v2, CLR(2)}, {v8, CLR(8)}, {v4, CLR(4)}},
+GLfloat Cube[12][3][2][3] = {	// 카메라 기준
+	{{v1, CLR(1)}, {v7, CLR(1)}, {v5, CLR(1)}},	//뒷면
+	{{v1, CLR(1)}, {v3, CLR(1)}, {v7, CLR(1)}},
+
+	{{v1, CLR(2)}, {v4, CLR(2)}, {v3, CLR(2)}},
+	{{v1, CLR(2)}, {v2, CLR(2)}, {v4, CLR(2)}},
+
+	{{v3, CLR(3)}, {v8, CLR(3)}, {v7, CLR(3)}},	// 윗면
+	{{v3, CLR(3)}, {v4, CLR(3)}, {v8, CLR(3)}},
+
+	{{v5, CLR(4)}, {v7, CLR(4)}, {v8, CLR(4)}},
+	{{v5, CLR(4)}, {v8, CLR(4)}, {v6, CLR(4)}},
+
+	{{v1, CLR(5)}, {v5, CLR(5)}, {v6, CLR(5)}},
+	{{v1, CLR(5)}, {v6, CLR(5)}, {v2, CLR(5)}},
+
+	{{v2, CLR(6)}, {v6, CLR(6)}, {v8, CLR(6)}},
+	{{v2, CLR(6)}, {v8, CLR(6)}, {v4, CLR(6)}},
 };
 
 GLfloat Pyramid[6][3][2][3] = {
-	{{v9, CLR(9)}, {v1, CLR(1)}, {v5, CLR(5)}},
-	{{v9, CLR(9)}, {v5, CLR(5)}, {v6, CLR(6)}},
-	{{v9, CLR(9)}, {v6, CLR(6)}, {v2, CLR(2)}},
-	{{v9, CLR(9)}, {v2, CLR(2)}, {v1, CLR(1)}},
-	{{v1, CLR(1)}, {v5, CLR(5)}, {v2, CLR(2)}},
-	{{v2, CLR(2)}, {v5, CLR(5)}, {v6, CLR(6)}},
+	{{v9, CLR(1)}, {v1, CLR(1)}, {v5, CLR(1)}},
+	{{v9, CLR(2)}, {v5, CLR(2)}, {v6, CLR(2)}},
+	{{v9, CLR(3)}, {v6, CLR(3)}, {v2, CLR(3)}},
+	{{v9, CLR(4)}, {v2, CLR(4)}, {v1, CLR(4)}},
+	{{v1, CLR(5)}, {v5, CLR(5)}, {v2, CLR(5)}},
+	{{v2, CLR(5)}, {v5, CLR(5)}, {v6, CLR(5)}},
 };
 
-GLfloat Lines[4][2][3] = {
-	{{0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 0.0f}},
-	{{0.0f, -1.0f, 0.0f}, {0.0f, 0.0f, 0.0f}},
-	{{-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}},
-	{{1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}},
+GLfloat Lines[6][2][3] = {
+	{{0.0f, 1.5f, 0.0f}, {0.0f, 0.0f, 0.0f}},
+	{{0.0f, -1.5f, 0.0f}, {0.0f, 0.0f, 0.0f}},
+	{{-1.5f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}},
+	{{1.5f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}},
+	{{0.0f, 0.0f, -1.5f}, {0.0f, 0.0f, 0.0f}},
+	{{0.0f, 0.0f, 1.5f}, {0.0f, 0.0f, 0.0f}},
 };
 
 int width;
@@ -121,6 +127,9 @@ int rotateX = 0;	// 0 : 정지	1 : 양의 방향 회전		2 : 음의 방향 회전
 int rotateY = 0;	// 0 : 정지	1 : 양의 방향 회전		2 : 음의 방향 회전
 bool isWire = false;
 bool isHide = true;
+int isCubeLidRotate = 0;
+int isCubeFrontOpen = 0;
+int isPyramidOpen = 0;
 
 GLuint vertexShader;
 GLuint fragmentShader;
@@ -128,7 +137,7 @@ GLuint fragmentShader;
 void make_vertexShaders() {
 	// 버텍스 세이더 읽어 저장하고 컴파일 하기
 	GLchar* vertexSource;
-	vertexSource = filetobuf("vertex6-12.glsl");
+	vertexSource = filetobuf("vertex7-16.glsl");
 
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexSource, NULL);
@@ -165,13 +174,16 @@ glm::mat4 transformMatrix(1.0f);
 glm::mat4 tx(1.0f);
 glm::mat4 ty(1.0f);
 glm::mat4 tz(1.0f);
-glm::mat4 ttx(1.0f);
-glm::mat4 tty(1.0f);
-glm::mat4 ttz(1.0f);
 glm::mat4 rx(1.0f);
 glm::mat4 ry(1.0f);
 
-unsigned int transformLocation;
+glm::mat4 lidRotateMatrix(1.0f);
+glm::mat4 frontRotateMatrix(1.0f);
+
+glm::mat4 backpyramidRotateMatrix(1.0f);
+glm::mat4 frontpyramidRotateMatrix(1.0f);
+glm::mat4 leftpyramidRotateMatrix(1.0f);
+glm::mat4 rightpyramidRotateMatrix(1.0f);
 
 void InitShader() {
 	make_vertexShaders();
@@ -189,6 +201,17 @@ void InitShader() {
 
 }
 
+unsigned int transformLocation;
+
+glm::mat4 projection = glm::mat4(1.0f);
+unsigned int projectionLocation;
+glm::mat4 view = glm::mat4(1.0f);
+unsigned int viewLocation;
+
+glm::vec3 cameraPos = glm::vec3(-2.0f, 2.0f, 3.0f);
+glm::vec3 cameraDirection = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
 void main(int argc, char** argv) {
 	width = 600;
 	height = 600;
@@ -197,7 +220,7 @@ void main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(width, height);
-	glutCreateWindow("practice12");
+	glutCreateWindow("practice16");
 
 	glewExperimental = GL_TRUE;
 	glewInit();
@@ -209,40 +232,83 @@ void main(int argc, char** argv) {
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 	glutKeyboardFunc(Keyboard);
-	glutSpecialFunc(SpecialKey);
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(Reshape);
 	glutTimerFunc(30, Timer, 1);
 
-	transformLocation = glGetUniformLocation(shaderID, "transform");
-
 	tx = glm::translate(tx, glm::vec3(-0.5f, 0.0f, 0.0f));
 	ty = glm::translate(ty, glm::vec3(0.0f, -0.5f, 0.0f));
 	tz = glm::translate(tz, glm::vec3(0.0f, 0.0f, -0.5f));
-	rx = glm::rotate(rx, glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	ry = glm::rotate(ry, glm::radians(-30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	rx = glm::rotate(rx, glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	ry = glm::rotate(ry, glm::radians(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	transformLocation = glGetUniformLocation(shaderID, "modelTransform");
+
+	projection = glm::perspective(glm::radians(45.0f), (float)GLUT_SCREEN_WIDTH / (float)GLUT_SCREEN_HEIGHT, 0.1f, 50.0f);
+	projectionLocation = glGetUniformLocation(shaderID, "projectionTransform");
+
+	view = glm::lookAt(cameraPos, cameraDirection, cameraUp);
+	viewLocation = glGetUniformLocation(shaderID, "viewTransform");
 
 	glutMainLoop();
 }
 
 GLvoid drawScene() {
+	static glm::mat4 tmp(1.0f);
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glUseProgram(shaderID);
 
+	glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, &projection[0][0]);
+	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &view[0][0]);
+
 	glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(transformMatrix));
 	if (type == 1) {
 		glBindVertexArray(VAO[0]);	// 삼각형의 속성이 있는 VAO[0] 사용
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glDrawArrays(GL_TRIANGLES, 0, 12);
+		// 윗면 돌리기
+		tmp = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -1.0f, -0.5f));
+		tmp = transformMatrix * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 1.0f, 0.5f)) * lidRotateMatrix * tmp;
+		glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(tmp));
+		glDrawArrays(GL_TRIANGLES, 12, 6);
+		// 윗면 돌리기 끝
+		glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(transformMatrix));
+		glDrawArrays(GL_TRIANGLES, 18, 12);
+
+		// 앞면 열기
+		tmp = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+		tmp = transformMatrix * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 1.0f)) * frontRotateMatrix * tmp;
+		glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(tmp));
+		glDrawArrays(GL_TRIANGLES, 30, 6);	// 앞면
 	}
 	else if (type == 2) {
 		glBindVertexArray(VAO[2]);	// 삼각형의 속성이 있는 VAO[0] 사용
-		glDrawArrays(GL_TRIANGLES, 0, 18);
+		tmp = glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, 0.0f, 0.0f));
+		tmp = transformMatrix * glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, 0.0f, 0.0f)) * backpyramidRotateMatrix * tmp;
+		glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(tmp));
+		glDrawArrays(GL_TRIANGLES, 0, 3);		// 뒷면
+
+		tmp = glm::translate(glm::mat4(1.0f), glm::vec3(-1.0f, 0.0f, 0.0f));
+		tmp = transformMatrix * glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.0f, 0.0f)) * rightpyramidRotateMatrix * tmp;
+		glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(tmp));
+		glDrawArrays(GL_TRIANGLES, 3, 3);		// 오른쪽
+
+		tmp = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+		tmp = transformMatrix * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 1.0f)) * frontpyramidRotateMatrix * tmp;
+		glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(tmp));
+		glDrawArrays(GL_TRIANGLES, 6, 3);		// 앞면
+
+		tmp = transformMatrix * leftpyramidRotateMatrix;
+		glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(tmp));
+		glDrawArrays(GL_TRIANGLES, 9, 3);		// 왼쪽
+
+		glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(transformMatrix));
+		glDrawArrays(GL_TRIANGLES, 12, 6);
 	}
 
 	glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(glm::mat4(1.0f)));
 	glBindVertexArray(VAO[1]);
-	glDrawArrays(GL_LINES, 0, 4);
+	glDrawArrays(GL_LINES, 0, 6);
 
 	glutSwapBuffers();
 }
@@ -295,33 +361,33 @@ GLvoid InitBuffer() {
 
 GLvoid Keyboard(unsigned char key, int x, int y) {
 	switch (key) {
-	case 'c':
+	case 'a':
 		type = 1;
 		break;
-	case 'p':
+	case 's':
 		type = 2;
 		break;
-	case 'h':
-		if (isHide) {
-			isHide = false;
-			glDisable(GL_DEPTH_TEST);
-		}
-		else {
-			isHide = true;
-			glEnable(GL_DEPTH_TEST);
-		}
+	case 't':
+		if (type == 1)
+			isCubeLidRotate = 1;
 		break;
-	case 'x':
-		rotateX = 1;
+	case 'T':
+		if (type == 1)
+			isCubeLidRotate = 0;
 		break;
-	case 'X':
-		rotateX = 2;
+	case 'f':
+		if (type == 1)
+			isCubeFrontOpen = 1;
+		break;
+	case 'F':
+		if (type == 1)
+			isCubeFrontOpen = 2;
 		break;
 	case 'y':
-		rotateY = 1;
-		break;
-	case 'Y':
-		rotateY = 2;
+		if (rotateY == 0)
+			rotateY = 1;
+		else
+			rotateY = 0;
 		break;
 	case 'w':
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -329,14 +395,22 @@ GLvoid Keyboard(unsigned char key, int x, int y) {
 	case 'W':
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 		break;
-	case 'S':
-		rotateX = 0;
-		rotateY = 0;
-		ttx = glm::mat4(1.0f);
-		tty = glm::mat4(1.0f);
-		ttz = glm::mat4(1.0f);
-		rx = glm::rotate(glm::mat4(1.0f), glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-		ry = glm::rotate(glm::mat4(1.0f), glm::radians(-30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	case 'o':
+		if (type == 2)
+			isPyramidOpen = 1;
+		break;
+	case 'O':
+		if (type == 2)
+			isPyramidOpen = 2;
+		break;
+	case 'p':
+		view = glm::mat4(1.0f);
+		projection = glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, -2.0f, 2.0f);
+		break;
+	case 'P':
+		view = glm::lookAt(cameraPos, cameraDirection, cameraUp);
+		projection = glm::perspective(glm::radians(45.0f), (float)GLUT_SCREEN_WIDTH / (float)GLUT_SCREEN_HEIGHT, 0.1f, 50.0f);
+		break;
 	}
 	glutPostRedisplay();
 }
@@ -354,25 +428,52 @@ GLvoid Timer(int value) {
 	else if (rotateY == 2) {
 		ry = glm::rotate(ry, glm::radians(-1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	}
-	transformMatrix = ttx * tty * ttz * rx * ry * tx * ty * tz;
+	static float FrontRad = 0.0f;
+	if (isCubeFrontOpen == 1) {
+		if (FrontRad < 90.0f) {
+			frontRotateMatrix = glm::rotate(frontRotateMatrix, glm::radians(1.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+			FrontRad += 1.0f;
+		}
+		else
+			isCubeFrontOpen = 0;
+	}
+	if (isCubeFrontOpen == 2) {
+		if (FrontRad > 0.0f) {
+			frontRotateMatrix = glm::rotate(frontRotateMatrix, glm::radians(-1.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+			FrontRad -= 1.0f;
+		}
+		else
+			isCubeFrontOpen = 0;
+	}
+	if (isCubeLidRotate == 1)
+		lidRotateMatrix = glm::rotate(lidRotateMatrix, glm::radians(1.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+	static float PyramidRad = 0.0f;
+
+	if (isPyramidOpen == 1) {
+		if (PyramidRad < 90.0f) {
+			backpyramidRotateMatrix = glm::rotate(backpyramidRotateMatrix, glm::radians(-1.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+			rightpyramidRotateMatrix = glm::rotate(rightpyramidRotateMatrix, glm::radians(-1.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+			frontpyramidRotateMatrix = glm::rotate(frontpyramidRotateMatrix, glm::radians(1.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+			leftpyramidRotateMatrix = glm::rotate(leftpyramidRotateMatrix, glm::radians(1.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+			PyramidRad += 1.0f;
+		}
+		else
+			isPyramidOpen == 0;
+	}
+	if (isPyramidOpen == 2) {
+		if (PyramidRad > 0.0f) {
+			backpyramidRotateMatrix = glm::rotate(backpyramidRotateMatrix, glm::radians(1.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+			rightpyramidRotateMatrix = glm::rotate(rightpyramidRotateMatrix, glm::radians(1.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+			frontpyramidRotateMatrix = glm::rotate(frontpyramidRotateMatrix, glm::radians(-1.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+			leftpyramidRotateMatrix = glm::rotate(leftpyramidRotateMatrix, glm::radians(-1.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+			PyramidRad -= 1.0f;
+		}
+		else
+			isPyramidOpen == 0;
+	}
+
+	transformMatrix =  rx * ry * tx * ty * tz;
 	glutPostRedisplay();
 	glutTimerFunc(30, Timer, 1);
-}
-
-GLvoid SpecialKey(int key, int x, int y) {
-	switch (key) {
-	case GLUT_KEY_UP:
-		tty = glm::translate(tty, glm::vec3(0.0f, 0.1f, 0.0f));
-		break;
-	case GLUT_KEY_DOWN:
-		tty = glm::translate(tty, glm::vec3(0.0f, -0.1f, 0.0f));
-		break;
-	case GLUT_KEY_LEFT:
-		ttx = glm::translate(ttx, glm::vec3(-0.1f, 0.0f, 0.0f));
-		break;
-	case GLUT_KEY_RIGHT:
-		ttx = glm::translate(ttx, glm::vec3(0.1f, 0.0f, 0.0f));
-		break;
-	}
-	glutPostRedisplay();
 }
