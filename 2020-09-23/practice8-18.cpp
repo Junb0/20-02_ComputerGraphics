a5c76376654be12a38799ccd7459c7112214ab63
junyeong <junb0sday@gmail.com>
2020-11-08 05:09:18 +0900

    8-17 완성, 8-18 거의 완성

--- 2020-09-23/practice8-18.cpp
+++ 2020-09-23/practice8-18.cpp
@@ -51,16 +51,19 @@ GLvoid UpdateBuffer();
 GLvoid Timer(int value);
 GLvoid Keyboard(unsigned char key, int x, int y);
 
-GLuint VAO[4], VBO[4];
+GLuint VAO[8], VBO[8];
 
 random_device rd;
 default_random_engine dre(rd());
 uniform_real_distribution<> urd1(-0.7f, 0.7f);
 uniform_real_distribution<> urd2(0.005f, 0.02f);
 uniform_real_distribution<> urdColor(0.0f, 1.0f);
-GLfloat clr[6][3] = {
-	rndclr,rndclr,rndclr,rndclr,rndclr,rndclr
+GLfloat clr[13][3] = {
+	{0.8f, 0.8f, 0.8f},{0.7f, 0.7f, 0.7f},{0.9f, 0.9f, 0.9f},{0.5f, 0.5f, 0.5f},{0.6f, 0.6f, 0.6f},{0.4f, 0.4f, 0.4f},
+	rndclr, rndclr, rndclr, rndclr, rndclr, rndclr, rndclr
 };
+
+int direction = UP;
 
 char* filetobuf(const char* file) {
 	FILE* fptr;
@@ -81,11 +84,11 @@ char* filetobuf(const char* file) {
 	return buf;
 }
 
-GLfloat Cube1[12][3][2][3] = {	// Ä«¸Þ¶ó ±âÁØ
+GLfloat Cube0[12][3][2][3] = {	// Àå¸·
 	{{v1, CLR(1)}, {v7, CLR(1)}, {v5, CLR(1)}},	//µÞ¸é
 	{{v1, CLR(1)}, {v3, CLR(1)}, {v7, CLR(1)}},
 	{{v1, CLR(1)}, {v4, CLR(1)}, {v3, CLR(1)}},
-	{{v1, CLR(1)}, {v2, CLR(1)}, {v4, CLR(1)}},	  							  
+	{{v1, CLR(1)}, {v2, CLR(1)}, {v4, CLR(1)}},
 	{{v3, CLR(1)}, {v8, CLR(1)}, {v7, CLR(1)}},	// À­¸é
 	{{v3, CLR(1)}, {v4, CLR(1)}, {v8, CLR(1)}},
 	{{v5, CLR(1)}, {v7, CLR(1)}, {v8, CLR(1)}},
@@ -96,52 +99,125 @@ GLfloat Cube1[12][3][2][3] = {	// Ä«¸Þ¶ó ±âÁØ
 	{{v2, CLR(1)}, {v8, CLR(1)}, {v4, CLR(1)}},
 };
 
-GLfloat Cube2[12][3][2][3] = {	// Ä«¸Þ¶ó ±âÁØ
-	{{v1, CLR(2)}, {v7, CLR(2)}, {v5, CLR(2)}},	//µÞ¸é
-	{{v1, CLR(2)}, {v3, CLR(2)}, {v7, CLR(2)}},
+GLfloat Cube1[12][3][2][3] = {	// ¹«´ë
+	{{v1, CLR(1)}, {v7, CLR(1)}, {v5, CLR(1)}},	//µÞ¸é
+	{{v1, CLR(1)}, {v3, CLR(1)}, {v7, CLR(1)}},
 	{{v1, CLR(2)}, {v4, CLR(2)}, {v3, CLR(2)}},
 	{{v1, CLR(2)}, {v2, CLR(2)}, {v4, CLR(2)}},
-	{{v3, CLR(2)}, {v8, CLR(2)}, {v7, CLR(2)}},	// À­¸é
-	{{v3, CLR(2)}, {v4, CLR(2)}, {v8, CLR(2)}},
-	{{v5, CLR(2)}, {v7, CLR(2)}, {v8, CLR(2)}},
-	{{v5, CLR(2)}, {v8, CLR(2)}, {v6, CLR(2)}},
-	{{v1, CLR(2)}, {v5, CLR(2)}, {v6, CLR(2)}},
-	{{v1, CLR(2)}, {v6, CLR(2)}, {v2, CLR(2)}},
-	{{v2, CLR(2)}, {v6, CLR(2)}, {v8, CLR(2)}},
-	{{v2, CLR(2)}, {v8, CLR(2)}, {v4, CLR(2)}},
-};
-
-GLfloat Cube3[12][3][2][3] = {	// Ä«¸Þ¶ó ±âÁØ
-	{{v1, CLR(3)}, {v7, CLR(3)}, {v5, CLR(3)}},	//µÞ¸é
-	{{v1, CLR(3)}, {v3, CLR(3)}, {v7, CLR(3)}},
-	{{v1, CLR(3)}, {v4, CLR(3)}, {v3, CLR(3)}},
-	{{v1, CLR(3)}, {v2, CLR(3)}, {v4, CLR(3)}},
 	{{v3, CLR(3)}, {v8, CLR(3)}, {v7, CLR(3)}},	// À­¸é
 	{{v3, CLR(3)}, {v4, CLR(3)}, {v8, CLR(3)}},
-	{{v5, CLR(3)}, {v7, CLR(3)}, {v8, CLR(3)}},
-	{{v5, CLR(3)}, {v8, CLR(3)}, {v6, CLR(3)}},
-	{{v1, CLR(3)}, {v5, CLR(3)}, {v6, CLR(3)}},
-	{{v1, CLR(3)}, {v6, CLR(3)}, {v2, CLR(3)}},
-	{{v2, CLR(3)}, {v6, CLR(3)}, {v8, CLR(3)}},
-	{{v2, CLR(3)}, {v8, CLR(3)}, {v4, CLR(3)}},
-};
-
-GLfloat Cube4[12][3][2][3] = {	// Ä«¸Þ¶ó ±âÁØ
-	{{v1, CLR(4)}, {v7, CLR(4)}, {v5, CLR(4)}},	//µÞ¸é
-	{{v1, CLR(4)}, {v3, CLR(4)}, {v7, CLR(4)}},
-	{{v1, CLR(4)}, {v4, CLR(4)}, {v3, CLR(4)}},
-	{{v1, CLR(4)}, {v2, CLR(4)}, {v4, CLR(4)}},
-	{{v3, CLR(4)}, {v8, CLR(4)}, {v7, CLR(4)}},	// À­¸é
-	{{v3, CLR(4)}, {v4, CLR(4)}, {v8, CLR(4)}},
 	{{v5, CLR(4)}, {v7, CLR(4)}, {v8, CLR(4)}},
 	{{v5, CLR(4)}, {v8, CLR(4)}, {v6, CLR(4)}},
-	{{v1, CLR(4)}, {v5, CLR(4)}, {v6, CLR(4)}},
-	{{v1, CLR(4)}, {v6, CLR(4)}, {v2, CLR(4)}},
-	{{v2, CLR(4)}, {v6, CLR(4)}, {v8, CLR(4)}},
-	{{v2, CLR(4)}, {v8, CLR(4)}, {v4, CLR(4)}},
+	{{v1, CLR(5)}, {v5, CLR(5)}, {v6, CLR(5)}},
+	{{v1, CLR(5)}, {v6, CLR(5)}, {v2, CLR(5)}},
+	{{v2, CLR(6)}, {v6, CLR(6)}, {v8, CLR(6)}},
+	{{v2, CLR(6)}, {v8, CLR(6)}, {v4, CLR(6)}},
 };
 
+GLfloat Cube2[12][3][2][3] = {	// ·Îº¿ ¸öÅë
+	{{v1, CLR(7)}, {v7, CLR(7)}, {v5, CLR(7)}},	//µÞ¸é
+	{{v1, CLR(7)}, {v3, CLR(7)}, {v7, CLR(7)}},
+	{{v1, CLR(7)}, {v4, CLR(7)}, {v3, CLR(7)}},
+	{{v1, CLR(7)}, {v2, CLR(7)}, {v4, CLR(7)}},
+	{{v3, CLR(7)}, {v8, CLR(7)}, {v7, CLR(7)}},	// À­¸é
+	{{v3, CLR(7)}, {v4, CLR(7)}, {v8, CLR(7)}},
+	{{v5, CLR(7)}, {v7, CLR(7)}, {v8, CLR(7)}},
+	{{v5, CLR(7)}, {v8, CLR(7)}, {v6, CLR(7)}},
+	{{v1, CLR(7)}, {v5, CLR(7)}, {v6, CLR(7)}},
+	{{v1, CLR(7)}, {v6, CLR(7)}, {v2, CLR(7)}},
+	{{v2, CLR(7)}, {v6, CLR(7)}, {v8, CLR(7)}},
+	{{v2, CLR(7)}, {v8, CLR(7)}, {v4, CLR(7)}},
+};
 
+GLfloat Cube3[12][3][2][3] = {	// ·Îº¿ ¸Ó¸®
+	{{v1, CLR(8)}, {v7, CLR(8)}, {v5, CLR(8)}},	//µÞ¸é
+	{{v1, CLR(8)}, {v3, CLR(8)}, {v7, CLR(8)}},
+	{{v1, CLR(8)}, {v4, CLR(8)}, {v3, CLR(8)}},
+	{{v1, CLR(8)}, {v2, CLR(8)}, {v4, CLR(8)}},
+	{{v3, CLR(8)}, {v8, CLR(8)}, {v7, CLR(8)}},	// À­¸é
+	{{v3, CLR(8)}, {v4, CLR(8)}, {v8, CLR(8)}},
+	{{v5, CLR(8)}, {v7, CLR(8)}, {v8, CLR(8)}},
+	{{v5, CLR(8)}, {v8, CLR(8)}, {v6, CLR(8)}},
+	{{v1, CLR(8)}, {v5, CLR(8)}, {v6, CLR(8)}},
+	{{v1, CLR(8)}, {v6, CLR(8)}, {v2, CLR(8)}},
+	{{v2, CLR(8)}, {v6, CLR(8)}, {v8, CLR(8)}},
+	{{v2, CLR(8)}, {v8, CLR(8)}, {v4, CLR(8)}},
+};
+
+GLfloat Cube4[12][3][2][3] = {	// ·Îº¿ ¿ÞÂÊ ´Ù¸®
+	{{v1, CLR(9)}, {v7, CLR(9)}, {v5, CLR(9)}},	//µÞ¸é
+	{{v1, CLR(9)}, {v3, CLR(9)}, {v7, CLR(9)}},
+	{{v1, CLR(9)}, {v4, CLR(9)}, {v3, CLR(9)}},
+	{{v1, CLR(9)}, {v2, CLR(9)}, {v4, CLR(9)}},
+	{{v3, CLR(9)}, {v8, CLR(9)}, {v7, CLR(9)}},	// À­¸é
+	{{v3, CLR(9)}, {v4, CLR(9)}, {v8, CLR(9)}},
+	{{v5, CLR(9)}, {v7, CLR(9)}, {v8, CLR(9)}},
+	{{v5, CLR(9)}, {v8, CLR(9)}, {v6, CLR(9)}},
+	{{v1, CLR(9)}, {v5, CLR(9)}, {v6, CLR(9)}},
+	{{v1, CLR(9)}, {v6, CLR(9)}, {v2, CLR(9)}},
+	{{v2, CLR(9)}, {v6, CLR(9)}, {v8, CLR(9)}},
+	{{v2, CLR(9)}, {v8, CLR(9)}, {v4, CLR(9)}},
+};
+
+GLfloat Cube5[12][3][2][3] = {	// ·Îº¿ ¿À¸¥ÂÊ ´Ù¸®
+	{{v1, CLR(10)}, {v7, CLR(10)}, {v5, CLR(10)}},	//µÞ¸é
+	{{v1, CLR(10)}, {v3, CLR(10)}, {v7, CLR(10)}},
+	{{v1, CLR(10)}, {v4, CLR(10)}, {v3, CLR(10)}},
+	{{v1, CLR(10)}, {v2, CLR(10)}, {v4, CLR(10)}},
+	{{v3, CLR(10)}, {v8, CLR(10)}, {v7, CLR(10)}},	// À­¸é
+	{{v3, CLR(10)}, {v4, CLR(10)}, {v8, CLR(10)}},
+	{{v5, CLR(10)}, {v7, CLR(10)}, {v8, CLR(10)}},
+	{{v5, CLR(10)}, {v8, CLR(10)}, {v6, CLR(10)}},
+	{{v1, CLR(10)}, {v5, CLR(10)}, {v6, CLR(10)}},
+	{{v1, CLR(10)}, {v6, CLR(10)}, {v2, CLR(10)}},
+	{{v2, CLR(10)}, {v6, CLR(10)}, {v8, CLR(10)}},
+	{{v2, CLR(10)}, {v8, CLR(10)}, {v4, CLR(10)}},
+};
+
+GLfloat Cube6[12][3][2][3] = {	// ·Îº¿ ¿ÞÂÊ ÆÈ
+	{{v1, CLR(11)}, {v7, CLR(11)}, {v5, CLR(11)}},	//µÞ¸é
+	{{v1, CLR(11)}, {v3, CLR(11)}, {v7, CLR(11)}},
+	{{v1, CLR(11)}, {v4, CLR(11)}, {v3, CLR(11)}},
+	{{v1, CLR(11)}, {v2, CLR(11)}, {v4, CLR(11)}},
+	{{v3, CLR(11)}, {v8, CLR(11)}, {v7, CLR(11)}},	// À­¸é
+	{{v3, CLR(11)}, {v4, CLR(11)}, {v8, CLR(11)}},
+	{{v5, CLR(11)}, {v7, CLR(11)}, {v8, CLR(11)}},
+	{{v5, CLR(11)}, {v8, CLR(11)}, {v6, CLR(11)}},
+	{{v1, CLR(11)}, {v5, CLR(11)}, {v6, CLR(11)}},
+	{{v1, CLR(11)}, {v6, CLR(11)}, {v2, CLR(11)}},
+	{{v2, CLR(11)}, {v6, CLR(11)}, {v8, CLR(11)}},
+	{{v2, CLR(11)}, {v8, CLR(11)}, {v4, CLR(11)}},
+};
+
+GLfloat Cube7[12][3][2][3] = {	// ·Îº¿ ¿À¸¥ÂÊ ÆÈ
+	{{v1, CLR(12)}, {v7, CLR(12)}, {v5, CLR(12)}},	//µÞ¸é
+	{{v1, CLR(12)}, {v3, CLR(12)}, {v7, CLR(12)}},
+	{{v1, CLR(12)}, {v4, CLR(12)}, {v3, CLR(12)}},
+	{{v1, CLR(12)}, {v2, CLR(12)}, {v4, CLR(12)}},
+	{{v3, CLR(12)}, {v8, CLR(12)}, {v7, CLR(12)}},	// À­¸é
+	{{v3, CLR(12)}, {v4, CLR(12)}, {v8, CLR(12)}},
+	{{v5, CLR(12)}, {v7, CLR(12)}, {v8, CLR(12)}},
+	{{v5, CLR(12)}, {v8, CLR(12)}, {v6, CLR(12)}},
+	{{v1, CLR(12)}, {v5, CLR(12)}, {v6, CLR(12)}},
+	{{v1, CLR(12)}, {v6, CLR(12)}, {v2, CLR(12)}},
+	{{v2, CLR(12)}, {v6, CLR(12)}, {v8, CLR(12)}},
+	{{v2, CLR(12)}, {v8, CLR(12)}, {v4, CLR(12)}},
+};
+
+GLfloat Cube8[12][3][2][3] = {	// ·Îº¿ ÄÚ
+	{{v1, CLR(13)}, {v7, CLR(13)}, {v5, CLR(13)}},	//µÞ¸é
+	{{v1, CLR(13)}, {v3, CLR(13)}, {v7, CLR(13)}},
+	{{v1, CLR(13)}, {v4, CLR(13)}, {v3, CLR(13)}},
+	{{v1, CLR(13)}, {v2, CLR(13)}, {v4, CLR(13)}},
+	{{v3, CLR(13)}, {v8, CLR(13)}, {v7, CLR(13)}},	// À­¸é
+	{{v3, CLR(13)}, {v4, CLR(13)}, {v8, CLR(13)}},
+	{{v5, CLR(13)}, {v7, CLR(13)}, {v8, CLR(13)}},
+	{{v5, CLR(13)}, {v8, CLR(13)}, {v6, CLR(13)}},
+	{{v1, CLR(13)}, {v5, CLR(13)}, {v6, CLR(13)}},
+	{{v1, CLR(13)}, {v6, CLR(13)}, {v2, CLR(13)}},
+	{{v2, CLR(13)}, {v6, CLR(13)}, {v8, CLR(13)}},
+	{{v2, CLR(13)}, {v8, CLR(13)}, {v4, CLR(13)}},
+};
 
 GLfloat Lines[6][2][3] = {
 	{{0.0f, 1.5f, 0.0f}, {0.0f, 0.0f, 0.0f}},
@@ -206,12 +282,17 @@ glm::mat4 box2Scale(1.0f);
 glm::mat4 box3Scale(1.0f);
 glm::mat4 box4Scale(1.0f);
 glm::mat4 box5Scale(1.0f);
+glm::mat4 noseScale(1.0f);
 
 glm::mat4 box1Translate(1.0f);
 glm::mat4 box2Translate(1.0f);
 glm::mat4 box3Translate(1.0f);
 glm::mat4 box4Translate(1.0f);
 glm::mat4 box5Translate(1.0f);
+glm::mat4 noseTranslate(1.0f);
+
+glm::mat4 leftArmTranslate(1.0f);
+glm::mat4 rightArmTranslate(1.0f);
 
 glm::mat4 machineTranslate(1.0f);
 glm::mat4 machineRotate(1.0f);
@@ -244,7 +325,7 @@ unsigned int projectionLocation;
 glm::mat4 view = glm::mat4(1.0f);
 unsigned int viewLocation;
 
-glm::vec3 cameraPos = glm::vec3(3.0f, 3.0f, 10.0f);
+glm::vec3 cameraPos = glm::vec3(0.0f, 4.0f, 20.0f);
 glm::vec3 cameraDirection = glm::vec3(0.0f, 0.0f, 0.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
@@ -269,6 +350,7 @@ void main(int argc, char** argv) {
 	InitBuffer();
 
 	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 	glutKeyboardFunc(Keyboard);
@@ -282,21 +364,29 @@ void main(int argc, char** argv) {
 	rx = glm::rotate(rx, glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 	ry = glm::rotate(ry, glm::radians(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
-	box1Scale = glm::scale(glm::mat4(1.0f), glm::vec3(10.0f, 1.0f, 10.0f));
-	box1Translate = glm::translate(glm::mat4(1.0f), glm::vec3(-5.0f, -1.0f, -5.0f));
+	box1Scale = glm::scale(glm::mat4(1.0f), glm::vec3(10.0f, 5.0f, 10.0f));
+	box1Translate = glm::translate(glm::mat4(1.0f), glm::vec3(-5.0f, 0.0f, -5.0f));
 
-	box2Scale = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 0.3f, 1.0f));
+	box2Scale = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 0.6f, 1.0f));
+	box2Translate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.7f, 0.0f));
 
 	box3Scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.7f, 0.4f, 0.7f));
-	box3Translate = glm::translate(glm::mat4(1.0f), glm::vec3(0.15f, 0.3f, 0.15f));
+	box3Translate = glm::translate(glm::mat4(1.0f), glm::vec3(0.15f, 1.3f, 0.15f));
 
-	box4Scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.2f, 0.7f, 0.2f));
-	box4Translate = glm::translate(glm::mat4(1.0f), glm::vec3(0.25f, 0.6f, 0.4f));
+	box4Scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f, 0.7f, 0.1f));
+	box4Translate = glm::translate(glm::mat4(1.0f), glm::vec3(0.25f, 0.0f, 0.45f));
 
-	box5Scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.2f, 0.7f, 0.2f));
-	box5Translate = glm::translate(glm::mat4(1.0f), glm::vec3(0.6f, 0.6f, 0.4f));
+	box5Scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f, 0.7f, 0.1f));
+	box5Translate = glm::translate(glm::mat4(1.0f), glm::vec3(0.6f, 0.0f, 0.45f));
+
+	leftArmTranslate = glm::translate(glm::mat4(1.0f), glm::vec3(-0.1f, 0.5f, 0.45f));
+
+	rightArmTranslate = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.5f, 0.45f));
 
 	machineTranslate = glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, 0.0f, -0.5f));
+
+	noseScale = glm::scale(glm::mat4(1.0f), glm::vec3(0.2f, 0.1f, 0.2f));
+	noseTranslate = glm::translate(glm::mat4(1.0f), glm::vec3(0.4f, 1.3f, 0.85f));
 
 	transformLocation = glGetUniformLocation(shaderID, "modelTransform");
 
@@ -318,12 +408,14 @@ GLvoid drawScene() {
 	glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, &projection[0][0]);
 	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &view[0][0]);
 
-	tmp = box1Translate *  box1Scale;
+	tmp = box1Translate * box1Scale;
 	glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(tmp));
 	glBindVertexArray(VAO[0]);	// »ï°¢ÇüÀÇ ¼Ó¼ºÀÌ ÀÖ´Â VAO[0] »ç¿ë
+	glCullFace(GL_FRONT);
 	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glCullFace(GL_BACK);
 
-	tmp = machineTranslate *  box2Translate * box2Scale;
+	tmp = machineTranslate * box2Translate * glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, 0.0f, 0.5f)) * machineRotate * glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, 0.0f, -0.5f)) * box2Scale;
 	glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(tmp));
 	glBindVertexArray(VAO[1]);
 	glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -333,13 +425,37 @@ GLvoid drawScene() {
 	glBindVertexArray(VAO[2]);
 	glDrawArrays(GL_TRIANGLES, 0, 36);
 
-	tmp = machineTranslate * box4Translate * glm::translate(glm::mat4(1.0f), glm::vec3(0.275f, 0.0f, 0.1f)) * machineRotate * glm::translate(glm::mat4(1.0f), glm::vec3(-0.275f, 0.0f, -0.1f)) * arm1Rotate * box4Scale;
+	//¿Þ´Ù¸®
+	tmp = machineTranslate * box4Translate * glm::translate(glm::mat4(1.0f), glm::vec3(0.275f, 0.0f, 0.05f)) * machineRotate * glm::translate(glm::mat4(1.0f), glm::vec3(-0.275f, 0.0f, -0.05f)) *
+		 glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.7f, 0.0f)) * arm1Rotate * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.7f, 0.0f)) * box4Scale;
 	glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(tmp));
 	glBindVertexArray(VAO[3]);
 	glDrawArrays(GL_TRIANGLES, 0, 36);
 
-	tmp = machineTranslate * box5Translate * glm::translate(glm::mat4(1.0f), glm::vec3(-0.075f, 0.0f, 0.1f)) * machineRotate * glm::translate(glm::mat4(1.0f), glm::vec3(0.075f, 0.0f, -0.1f)) * arm2Rotate * box5Scale;
+	//¿À¸¥´Ù¸®
+	tmp = machineTranslate * box5Translate * glm::translate(glm::mat4(1.0f), glm::vec3(-0.075f, 0.0f, 0.05f)) * machineRotate * glm::translate(glm::mat4(1.0f), glm::vec3(0.075f, 0.0f, -0.05f)) *
+		glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.7f, 0.0f)) * arm2Rotate * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.7f, 0.0f)) * box5Scale;
 	glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(tmp));
+	glBindVertexArray(VAO[4]);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	//¿ÞÆÈ
+	tmp = machineTranslate * leftArmTranslate * glm::translate(glm::mat4(1.0f), glm::vec3(0.55f, 0.0f, 0.05f)) * machineRotate * glm::translate(glm::mat4(1.0f), glm::vec3(-0.55f, 0.0f, -0.05f)) *
+		glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.7f, 0.0f)) * arm2Rotate * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.7f, 0.0f)) * box4Scale;
+	glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(tmp));
+	glBindVertexArray(VAO[5]);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	//¿À¸¥ÆÈ
+	tmp = machineTranslate * rightArmTranslate * glm::translate(glm::mat4(1.0f), glm::vec3(-0.45f, 0.0f, 0.05f)) * machineRotate * glm::translate(glm::mat4(1.0f), glm::vec3(0.45f, 0.0f, -0.05f)) *
+		glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.7f, 0.0f)) * arm1Rotate * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.7f, 0.0f)) * box4Scale;
+	glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(tmp));
+	glBindVertexArray(VAO[6]);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	//ÄÚ
+	tmp = machineTranslate * noseTranslate * glm::translate(glm::mat4(1.0f), glm::vec3(0.1f, 0.0f, 0.1f)) * machineRotate * glm::translate(glm::mat4(1.0f), glm::vec3(-0.1f, 0.0f, -0.1f)) * noseScale;
+	glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(tmp));
+	glBindVertexArray(VAO[7]);
 	glDrawArrays(GL_TRIANGLES, 0, 36);
 
 	glutSwapBuffers();
@@ -350,8 +466,8 @@ GLvoid Reshape(int w, int h) {							//---ÄÝ¹éÇÔ¼ö : ´Ù½Ã ±×¸®±â ÄÝ¹é ÇÔ¼ö
 }
 
 GLvoid InitBuffer() {
-	glGenVertexArrays(4, VAO);
-	glGenBuffers(4, VBO);
+	glGenVertexArrays(8, VAO);
+	glGenBuffers(8, VBO);
 
 	// µµÇü VAO
 	glBindVertexArray(VAO[0]);
@@ -391,6 +507,50 @@ GLvoid InitBuffer() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO[3]);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Cube4), Cube4, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(VAO[4]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[4]);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Cube5), Cube5, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(VAO[5]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[5]);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Cube6), Cube6, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(VAO[6]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[6]);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Cube7), Cube7, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(VAO[7]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[7]);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Cube8), Cube8, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -460,7 +620,7 @@ GLvoid Keyboard(unsigned char key, int x, int y) {
 		break;
 	case 'r':
 		cameraRotate = glm::rotate(cameraRotate, glm::radians(1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		view =  glm::lookAt(cameraPos, cameraDirection, cameraUp) * cameraRotate;
+		view = glm::lookAt(cameraPos, cameraDirection, cameraUp) * cameraRotate;
 		break;
 	case 'R':
 		cameraRotate = glm::rotate(cameraRotate, glm::radians(-1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
