d4b7e6b7c082629cb4d7eafcc4e2875f9b23d988
Junyeong Park <junb0sday@gmail.com>
2020-11-09 21:56:47 +0900

    쉐이더 변경

--- ReadObj/fragment.glsl
+++ ReadObj/fragment.glsl
@@ -1,8 +1,9 @@
 #version 330 core
 
-in vec3 out_position;
-in vec3 out_normal;
-out vec4 FragColor;
+in vec3 v2f_normal;
+in vec3 v2f_worldPos; 
+
+out vec4 out_color;
 
 uniform vec3 g_lightAmbient;
 uniform vec3 g_lightPos;
@@ -10,24 +11,23 @@ uniform vec3 g_lightColor;
 uniform vec3 g_objectColor;
 uniform vec3 g_cameraPos;
 
-void main(){
-	// Ambient
+void main(void)
+{	
 	vec3 ambient = g_lightAmbient * g_lightColor;
 
-	// Diffuse
-	vec3 normalVector = normalize(out_normal);
-	vec3 lightDir = normalize(g_lightPos - out_position);
-	float diffuseLight = max(dot(normalVector, lightDir), 0.0);
+	vec3 normalVector = normalize(v2f_normal);
+	vec3 lightDir = normalize(g_lightPos - v2f_worldPos);
+	float diffuseLight = max(dot(v2f_normal, lightDir), 0.0);
 	vec3 diffuse = diffuseLight * g_lightColor;
 
-	// Specular
 	int shininess = 128;
-	vec3 viewDir = normalize(g_cameraPos - out_position);
+	vec3 viewDir = normalize(g_cameraPos - v2f_worldPos);
 	vec3 reflectDir = reflect(-lightDir, normalVector);
-	float specularLight = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+	float specularLight = max(dot(viewDir, reflectDir), 0.0);
+	specularLight = pow(specularLight, shininess);
 	vec3 specular = specularLight * g_lightColor;
 
-	vec3 result = (diffuse + specular + ambient) * g_objectColor;
+	vec3 result = (ambient + diffuse + specular) * g_objectColor;
 
-	FragColor = vec4(result, 1.0);
+	out_color = vec4(result, 1.0);
 }
--- ReadObj/vertex.glsl
+++ ReadObj/vertex.glsl
@@ -1,19 +1,22 @@
-#version 330 core
+#version 330
 
 in vec3 in_position;
 in vec3 in_normal;
 in vec2 in_uv;
 
-out vec3 out_position;
-out vec3 out_normal;
-out vec2 out_uv;
+out vec3 v2f_normal;
+out vec3 v2f_worldPos; 
 
 uniform mat4 g_modelTransform;
 uniform mat4 g_view;
 uniform mat4 g_projection;
 
-void main(){
-	gl_Position = g_projection * g_view * g_modelTransform * vec4(in_position, 1.0);
-	out_position = vec3(g_modelTransform * vec4(in_position, 1.0));
-	out_normal = mat3(transpose(inverse(g_modelTransform))) * in_normal;
+void main(void)
+{
+	vec4 worldPos = g_modelTransform * vec4(in_position, 1.0);
+	gl_Position = g_projection * g_view * worldPos;
+
+	vec4 worldNormal = g_modelTransform * vec4(in_normal, 0.0);
+	v2f_normal = vec3(worldNormal.x, worldNormal.y, worldNormal.z);
+	v2f_worldPos = vec3(worldPos.x, worldPos.y, worldPos.z);
 }
